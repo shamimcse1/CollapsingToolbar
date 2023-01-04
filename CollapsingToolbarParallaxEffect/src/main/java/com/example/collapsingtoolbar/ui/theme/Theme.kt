022008da930cb49d4ab2c08d75c7e1f34b9fbe78
Md. Abdullah <abdullah@appinionbd.com>
2023-01-04 15:51:41 +0600

    Initial commit

---
+++ CollapsingToolbarParallaxEffect/src/main/java/com/example/collapsingtoolbar/ui/theme/Theme.kt
@@ -0,0 +1,23 @@
+package com.example.collapsingtoolbar.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun CollapsingToolbarTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
